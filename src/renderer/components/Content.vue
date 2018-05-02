933afea2a256bcf97ae7b3c073e516e263ee59ae
Tobias Walter <tobias.walter@gmx.net>
2018-05-02 15:11:38 +0200

    filter drafts by clicking in the new toolbar

--- src/renderer/components/Content.vue
+++ src/renderer/components/Content.vue
@@ -1,45 +1,79 @@
 <template>
-  <section class="section">
-    <h1 class="title">Posts</h1>
-    <div class="columns is-multiline">
-
-      <div class="column is-12" v-for="post in posts" :key="post.title">
-        <article class="box">
-          <div class="media">
-            <aside class="media-left has-text-centered">
-              <img :src="post.titleImage" width="80">
-              <br>
-              <span class="tag is-warning" v-if="post.draft">Draft</span>
-            </aside>
-            <div class="media-content">
-              <p class="title is-5 is-marginless">
-                <a @click="openPost(post)">{{post.title}}</a>
-              </p>
-              <div class="subtitle is-marginless">
-                <span class="tag is-primary" v-for="category in post.categories" :key="category">{{category}}</span>
-              </div>
-              <div class="blog-tags">
-                <span class="tag" v-for="tag in post.tags" :key="tag">{{tag}}</span>
-              </div>
-              <p class="content is-small">
-                <a @click="openPost(post)">Edit</a>
-                <span>·</span>
-                <a>Delete</a>
-                <span class="is-pulled-right" v-if="post.draft">Last edited {{post.date.toLocaleDateString()}}</span>
-                <span class="is-pulled-right" v-if="!post.draft">Published {{post.date.toLocaleDateString()}}</span>
-              </p>
-            </div>
+  <div>
+    <div class="container">
+      <nav class="navbar is-transparent">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://github.com/PapaMufflon/hugo-desktop">
+            hugo-desktop
+          </a>
+        </div>
+        
+        <div id="navMenu" class="navbar-menu">
+          <div class="navbar-start">
+              
           </div>
-        </article>
-      </div>
 
+          <div class="navbar-end">
+            <a class="navbar-item" @click="toggleDraftFilter">
+              <span class="icon" style="color: #333;">
+                <font-awesome-icon :icon="['fas', 'columns']" />
+              </span>
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
-  </section>
+    <section class="section">
+      <h1 class="title">Posts</h1>
+      <div class="columns is-multiline">
+
+        <div
+          class="column is-12"
+          v-for="post in posts"
+          :key="post.title"
+          v-if="!filterDrafts || !post.draft">
+          <article class="box">
+            <div class="media">
+              <aside class="media-left has-text-centered">
+                <img :src="post.titleImage" width="80">
+                <br>
+                <span class="tag is-warning" v-if="post.draft">Draft</span>
+              </aside>
+              <div class="media-content">
+                <p class="title is-5 is-marginless">
+                  <a @click="openPost(post)">{{post.title}}</a>
+                </p>
+                <div class="subtitle is-marginless">
+                  <span class="tag is-primary" v-for="category in post.categories" :key="category">{{category}}</span>
+                </div>
+                <div class="blog-tags">
+                  <span class="tag" v-for="tag in post.tags" :key="tag">{{tag}}</span>
+                </div>
+                <p class="content is-small">
+                  <a @click="openPost(post)">Edit</a>
+                  <span>·</span>
+                  <a>Delete</a>
+                  <span class="is-pulled-right" v-if="post.draft">Last edited {{post.date.toLocaleDateString()}}</span>
+                  <span class="is-pulled-right" v-if="!post.draft">Published {{post.date.toLocaleDateString()}}</span>
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'editor',
+    data: function () {
+      return {
+        filterDrafts: false
+      }
+    },
     computed: {
       posts () {
         return this.$store.getters.sortedPosts
@@ -53,6 +87,9 @@
             post: post
           }
         })
+      },
+      toggleDraftFilter: function () {
+        this.filterDrafts = !this.filterDrafts
       }
     }
   }
